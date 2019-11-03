3540966052761052346b56c5e872e951b8f534ec
Abby Davidow <lenexapaws@gmail.com>
2019-11-03 14:28:17 -0600

    Added documentation to game 3 and backtohome in game1

--- game1/checkers.js
+++ game1/checkers.js
@@ -219,6 +219,11 @@ function resetOptions(){
 	table.canJump = false;
 }
 
+/**
+* backHome()
+* pre: the HTML button exists
+* post: Takes the user back to the home page of the arcade
+*/
 function backHome(){
   window.location.replace("https://people.eecs.ku.edu/~a035d579/eecs-448-project3and4/homePage.html");
 }
--- game3/memoryGame.js
+++ game3/memoryGame.js
@@ -1,15 +1,20 @@
 const cards = document.querySelectorAll('.memory-card');
 
+/*
+*flipCard()
+*Not finished yet, but will end up controlling what happens when a card is flipped
+*/
 function flipCard(){
   this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
 
-
-
-
-
+/**
+* backHome()
+* pre: the HTML button exists
+* post: Takes the user back to the home page of the arcade
+*/
 function backHome(){
   window.location.replace("https://people.eecs.ku.edu/~a035d579/eecs-448-project3and4/homePage.html");
 }
