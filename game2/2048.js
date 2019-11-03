e438c29623ef6ed1db11e778edc1fd77136e74b7
Abby Davidow <lenexapaws@gmail.com>
2019-11-02 22:20:25 -0500

    Added back to homepage button to games 1-3

--- game2/2048.js
+++ game2/2048.js
@@ -1,1 +1,3 @@
-hello
+function backHome(){
+  window.location.replace("https://people.eecs.ku.edu/~a035d579/eecs-448-project3and4/homePage.html");
+}
--- game3/memoryGame.js
+++ game3/memoryGame.js
@@ -5,3 +5,11 @@ function flipCard(){
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
+
+
+
+
+
+function backHome(){
+  window.location.replace("https://people.eecs.ku.edu/~a035d579/eecs-448-project3and4/homePage.html");
+}
