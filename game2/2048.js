2280b5ca049f943b6a000e9d29446bdf8d144bee
Abby Davidow <lenexapaws@gmail.com>
2019-11-03 19:16:26 -0600

    Added documentation to game 2

--- game2/2048.js
+++ game2/2048.js
@@ -1,3 +1,8 @@
+/**
+* backHome()
+* pre: the HTML button exists
+* post: Takes the user back to the home page of the arcade
+*/
 function backHome(){
   window.location.replace("https://people.eecs.ku.edu/~a035d579/eecs-448-project3and4/homePage.html");
 }
