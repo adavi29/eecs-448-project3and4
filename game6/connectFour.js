35a910bd0e879db871ad3e89cb01ff23bd120259
Anissa Khan <anissa.khan@gmail.com>
2019-11-17 20:14:36 -0600

    connect4 diagonal win possible

--- game6/connectFour.js
+++ game6/connectFour.js
@@ -1,7 +1,13 @@
 let playerRed=true; //red
 let playerYellow=false; //yellow
-let winner="";
 
+/**
+* post: Manages gameplay
+* post: Creates the table and adds cell attributes
+* post: Defines functions: cell.onclick and an anonymous function that is called when the new game button is clicked
+* cell.onclick() calls the click function when a cell is clicked
+* the anonymous function deletes the table when 'new game' is clicked and then calls ticTacToe() function again to start a new game.
+*/
 function connectFour() {
     table = document.getElementById("gameBoard");
 
@@ -16,15 +22,9 @@ function connectFour() {
             cell.hasValue = false;
             cell.isRed = false;
             cell.isYellow = false;
-            //cell.adjCounter=0;
-
-            /*col.onclick = function(){
-                document.getElementById(j).style.backgroundColor="beige";
-            };*/
 
             cell.onclick =  function(){
                 //this.style.backgroundColor="beige";
-                //document.getElementById(i*7+j).style.backgroundColor="beige";
                 click(this, j);
             };
 
@@ -38,6 +38,13 @@ function connectFour() {
         connectFour()};
 }
 
+/**
+* pre: is called when a table cell is clicked
+* post: places a piece on the board and checks for winners
+* exception: alert is called when user tries to click on a column that is full
+* return: returns when a player wins
+* param: cell: the cell that is affected when click is called
+*/
 function click(cell, col){
     //console.log("clicked cell # "+cell.id);
     if(cell.hasValue)
@@ -70,6 +77,13 @@ function click(cell, col){
     switchPlayer();
 }
 
+/**
+* pre: a cell is clicked
+* post: updates the table so that when a column is clicked, the next available cell at the bottom of the table is filled
+* return: returns the cell that is actually affected: the cell where a piece is placed
+* param: cell: the cell that is clicked. This changes by the end of the function to return the cell where the piece is placed
+* param: col: the column in which the cell that is clicked lives
+*/
 function selectCell(cell, col){
     for(let i=0; i<5; i++)
     {
@@ -120,11 +134,17 @@ function switchPlayer(){
     }
 }
 
+/**
+* pre: a cell is clicked
+* post: checks for a winner and returns true if winner exists; false otherwise
+* return: returns true when the click results in a win; false otherwise
+*/
 function winChoice(){
-    let winCountHoriz=checkHoriz();
-    let winCountVert=checkVertical();
-    let winCountDiag=checkDiag();
-    if(winCountHoriz>=4 || winCountVert>=4 || winCountDiag>=4)
+    let winCountHoriz=checkHoriz(); //count
+    let winCountVert=checkVertical(); //count
+    let winDiagUp=checkDiagUp(); //bool
+    let winDiagDown=checkDiagDown(); //bool
+    if(winCountHoriz>=4 || winCountVert>=4 || winDiagUp==true || winDiagDown==true)
     {
         return(true);
     }
@@ -134,6 +154,11 @@ function winChoice(){
     }
 }
 
+/**
+* pre: a cell is clicked
+* post: checks for a vertical winner: checks for 4 vertically adjacent matching pieces
+* return: returns the winCount, which is a counter for how many vertically adjacent cells there are. either returns 4 or 0.
+*/
 function checkVertical(){
     let winCount=0;
     let cell=table.rows[0].cells[0];
@@ -167,6 +192,11 @@ function checkVertical(){
     return(winCount);
 }
 
+/**
+* pre: a cell is clicked
+* post: checks for a horizontal winner: checks for 4 horizontally adjacent matching pieces
+* return: returns the winCount, which is a counter for how many horizontally adjacent cells there are. either returns 4 or 0.
+*/
 function checkHoriz(){
     let winCount=0;
     let cell=table.rows[0].cells[0];
@@ -201,19 +231,71 @@ function checkHoriz(){
     return(winCount);
 }
 
-function checkDiag(){
-
-}
-
-/*function adjustCounters(cell){
-    for(let i=0; i<6; i++)
+/**
+* pre: a cell is clicked
+* post: checks for a diagonal winner: checks for 4 diagonally adjacent matching pieces going from left to bottom right
+* return: returns true if 4 consecutive diagonal pieces are on the board; false otherwise
+*/
+function checkDiagDown(){
+    let cell=table.rows[0].cells[0];
+    for(let i=3; i<6; i++)
     {
-        for(let i=0; i<7; i++)
+        for(let j=0; j<4; j++)
         {
+            cell=table.rows[i].cells[j];
 
+            if(playerRed==true)
+            {
+                if(cell.isRed && table.rows[i-1].cells[j+1].isRed && table.rows[i-2].cells[j+2].isRed && table.rows[i-3].cells[j+3].isRed)
+                {
+                   return(true);
+                }
+            }
+            else
+            {
+                if(cell.isYellow && table.rows[i-1].cells[j+1].isYellow && table.rows[i-2].cells[j+2].isYellow && table.rows[i-3].cells[j+3].isYellow)
+                {
+                   return(true);
+                }
+            }
+            
         }
     }
-}*/
+    return(false);
+}
+
+/**
+* pre: a cell is clicked
+* post: checks for a diagonal winner: checks for 4 diagonally adjacent matching pieces going from right to top left
+* return: returns true if 4 consecutive diagonal pieces are on the board; false otherwise
+*/
+function checkDiagUp(){
+    let cell=table.rows[0].cells[0];
+    for(let i=3; i<6; i++)
+    {
+        for(let j=3; j<7; j++)
+        {
+            cell=table.rows[i].cells[j];
+
+            if(playerRed==true)
+            {
+                if(cell.isRed && table.rows[i-1].cells[j-1].isRed && table.rows[i-2].cells[j-2].isRed && table.rows[i-3].cells[j-3].isRed)
+                {
+                   return(true);
+                }
+            }
+            else
+            {
+                if(cell.isYellow && table.rows[i-1].cells[j-1].isYellow && table.rows[i-2].cells[j-2].isYellow && table.rows[i-3].cells[j-3].isYellow)
+                {
+                   return(true);
+                }
+            }
+            
+        }
+    }
+    return(false);
+}
 
 /**
 * pre: the board is full
@@ -229,3 +311,11 @@ function printWinner(){
         alert("Player Yellow wins!");
     }
 }
+
+/**
+* pre: the HTML button exists
+* post: Takes the user back to the home page of the arcade
+*/
+function backHome(){
+    window.location.replace("../homePage.html");
+  }
