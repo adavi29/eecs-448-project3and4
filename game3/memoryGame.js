ec66217b39fe32a5374a3a50c6e0e5086f4142fa
Abby Davidow <lenexapaws@gmail.com>
2019-11-20 18:14:30 -0600

    Added documentation to game3

--- game3/memoryGame.js
+++ game3/memoryGame.js
@@ -8,8 +8,10 @@ let numCorrect=0;
 cards.forEach(card => card.addEventListener('click', flipCard));
 shuffleCards();
 
-/*
-*Not finished yet, but will end up controlling what happens when a card is flipped
+/**
+*Post: assigns clicked on cards to firstCard, secondCard, and thirdCard based on how many cards have already been flipped.
+*Also checks that the card hasn't been clicked before.
+*Once all three cards have been assigned, it calls function to check for a match
 */
 function flipCard(){
   if(lockBoard){
@@ -37,6 +39,12 @@ function flipCard(){
   checkForMatch();
 }
 
+/**
+*Pre: Three cards have been chosen
+*Post: Determines whether the three cards are matches.
+*Disables cards if they match and unlfips them if they don't.
+*Also tracks how many are correct to alert the user when they win.
+*/
 function checkForMatch(){
   if(firstCard.dataset.framework == secondCard.dataset.framework && firstCard.dataset.framework == thirdCard.dataset.framework && thirdCard.dataset.framework == secondCard.dataset.framework){
     disableCards();
@@ -49,13 +57,21 @@ function checkForMatch(){
   unflipCards();
 }
 
+/**
+*Pre: The three cards chosen all match
+*Post: Removes event listeners on the three cards and resets board
+*/
 function disableCards(){
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
+  thirdCard.removeEventListener('click', flipCard);
   resetBoard();
 }
 
+/**
+*Pre: The three cards chosen do not match
+*Post: Locks board, unflips the three cards, and resets board.
+*/
 function unflipCards(){
   lockBoard=true;
   setTimeout(() => {
@@ -65,6 +81,10 @@ function unflipCards(){
     resetBoard();}, 1200);
 }
 
+/**
+*Pre: Cards have either been disabled or unflipped
+*Post: Resets the following variables: hasFlippedCard, hasFlippedSecondCard, lockBoard, firstCard, secondCard, and thirdCard.
+*/
 function resetBoard(){
   hasFlippedCard=false;
   hasFlippedSecondCard=false;
@@ -72,6 +92,10 @@ function resetBoard(){
   firstCard,secondCard,thirdCard=null;
 }
 
+/**
+*Pre: card variable has been declared
+*Post: shuffles the order id's for all cards to shuffle them
+*/
 function shuffleCards(){
   cards.forEach(card => {
     let random = Math.floor(Math.random() * 12);
@@ -79,6 +103,10 @@ function shuffleCards(){
   });
 }
 
+/**
+*Pre: User clicks new game.
+*Post: Resets all variables, unflips all cards, resets card event listeners, and shuffles cards after 1000 ms.
+*/
 function clearBoard(){
   document.getElementById('youWin').innerText = "";
   hasFlippedCard=false;
@@ -94,19 +122,22 @@ function clearBoard(){
   });
   setTimeout(shuffleCards, 1000);
 }
+
 /**
-* pre: the HTML button exists
-* post: Takes the user back to the home page of the arcade
+* Pre: the HTML button exists
+* Post: Takes the user back to the home page of the arcade
 */
 function backHome(){
   window.location.replace("../homePage.html");
 }
 
+/**
+*Post: Runs all tests.
+*/
 function runMemoryTests(){
-  //test 1 on click card turns
   document.getElementById("card1").click()
-  let test1 = "Test 1: hasFlippedCard is true when a card is clicked: "
-  if(hasFlippedCard==true){
+  let test1 = "Test 1: hasFlippedCard is true and hasFlippedSecondCard is false when one card is clicked: "
+  if(hasFlippedCard==true && hasFlippedSecondCard==false){
     document.getElementById("test1").innerText = test1 + "PASSED";
   } else {
     document.getElementById("test1").innerText = test1 + "FAILED";
